2a34feacc2c0a5cd888a6be208fd0f0e72aba1ae
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-06-17 23:55:55 +0200

    add map and unmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate xenforeignmemory_sys;
+use std::io::Error;
 use std::ptr::{null_mut};
+use std::os::raw::{c_int, c_ulong, c_void};
 
 #[derive(Debug)]
 pub struct XenForeignMem {
@@ -18,6 +20,27 @@ impl XenForeignMem {
         return Ok(XenForeignMem {
             handle: fn_handle,
         });
+    }
+
+    pub fn map(&self, domid: u32, prot: c_int, gfn: u64) -> Result<*mut c_void,&str>{
+        let arr_gfn: [c_ulong; 1] = [gfn];
+        let map = unsafe {
+            xenforeignmemory_sys::xenforeignmemory_map(self.handle, domid, prot, arr_gfn.len(), arr_gfn.as_ptr(), null_mut())
+        };
+        if map.is_null() {
+            return Err("Fail to map gfn");
+        }
+        Ok(map)
+    }
+
+    pub fn unmap(&self, addr: *mut c_void) -> Result<(),Error>{
+        let result = unsafe {
+            xenforeignmemory_sys::xenforeignmemory_unmap(self.handle, addr, 1)
+        };
+        match result {
+            0 => Ok(()),
+            _ => Err(Error::last_os_error())
+        }
     }
 
     fn close(&mut self) -> Result<(),&str> {
