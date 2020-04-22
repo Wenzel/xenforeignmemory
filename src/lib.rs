908a641b9226512bee17ad7ce13a98f1a66cd2f7
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-22 18:47:28 +0200

    load libxenforeignmemory at runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,43 @@
+mod libxenforeignmemory;
+
 extern crate xenforeignmemory_sys;
+
 use std::io::Error;
-use std::ptr::{null_mut};
-use std::os::raw::{c_int, c_ulong, c_void, c_uchar};
+use std::os::raw::{c_int, c_uchar, c_ulong, c_void};
+use std::ptr::null_mut;
 use std::slice;
+
+use libxenforeignmemory::LibXenForeignMemory;
 
 #[derive(Debug)]
 pub struct XenForeignMem {
     handle: *mut xenforeignmemory_sys::xenforeignmemory_handle,
+    libxenforeignmemory: LibXenForeignMemory,
 }
 
 impl XenForeignMem {
-
-    pub fn new() -> Result<Self,String> {
-        let fn_handle = unsafe {
-            xenforeignmemory_sys::xenforeignmemory_open(null_mut(), 0)
-        };
+    pub fn new() -> Result<Self, String> {
+        let libxenforeignmemory = unsafe { LibXenForeignMemory::new() };
+        let fn_handle = (libxenforeignmemory.open)(null_mut(), 0);
         if fn_handle == null_mut() {
             return Err("Fail to open xenforeignmemory interface".to_string());
         }
         return Ok(XenForeignMem {
             handle: fn_handle,
+            libxenforeignmemory,
         });
     }
 
-    pub fn map(&self, domid: u32, prot: c_int, gfn: u64) -> Result<&mut [u8],&str>{
+    pub fn map(&self, domid: u32, prot: c_int, gfn: u64) -> Result<&mut [u8], &str> {
         let arr_gfn: [c_ulong; 1] = [gfn];
-        let map = unsafe {
-            xenforeignmemory_sys::xenforeignmemory_map(self.handle, domid, prot, arr_gfn.len(), arr_gfn.as_ptr(), null_mut())
-        } as *mut c_uchar;
+        let map = (self.libxenforeignmemory.map)(
+            self.handle,
+            domid,
+            prot,
+            arr_gfn.len(),
+            arr_gfn.as_ptr(),
+            null_mut(),
+        ) as *mut c_uchar;
         if map.is_null() {
             return Err("Fail to map gfn");
         }
@@ -37,21 +47,17 @@ impl XenForeignMem {
         Ok(unsafe { slice::from_raw_parts_mut(map, 4096 as usize) })
     }
 
-    pub fn unmap(&self, page: &mut [u8]) -> Result<(),Error>{
+    pub fn unmap(&self, page: &mut [u8]) -> Result<(), Error> {
         let addr = page.as_mut_ptr() as *mut c_void;
-        let result = unsafe {
-            xenforeignmemory_sys::xenforeignmemory_unmap(self.handle, addr, 1)
-        };
+        let result = (self.libxenforeignmemory.unmap)(self.handle, addr, 1);
         match result {
             0 => Ok(()),
-            _ => Err(Error::last_os_error())
+            _ => Err(Error::last_os_error()),
         }
     }
 
-    fn close(&mut self) -> Result<(),&str> {
-        let result = unsafe {
-            xenforeignmemory_sys::xenforeignmemory_close(self.handle)
-        };
+    fn close(&mut self) -> Result<(), &str> {
+        let result = (self.libxenforeignmemory.close)(self.handle);
         match result {
             0 => Ok(()),
             _ => Err("Fail to close xenforeignmemory interface"),
---
+++ src/libxenforeignmemory.rs
@@ -0,0 +1,61 @@
+use std::os::raw::{c_int, c_uint, c_void};
+
+use xenforeignmemory_sys::{xen_pfn_t, xenforeignmemory_handle, xentoollog_logger};
+
+use libloading::os::unix::Symbol as RawSymbol;
+use libloading::{Library, Symbol};
+use log::info;
+
+const LIBXENFOREIGNMEMORY_FILENAME: &'static str = "libxenforeignmemory.so";
+// xenforeignmemory_open
+type FnOpen =
+    fn(logger: *mut xentoollog_logger, open_flags: c_uint) -> *mut xenforeignmemory_handle;
+// xenforeignmemory_close
+type FnClose = fn(fmem: *mut xenforeignmemory_handle) -> c_int;
+// xenforeignmemory_map
+type FnMap = fn(
+    fmem: *mut xenforeignmemory_handle,
+    dom: u32,
+    prot: c_int,
+    pages: usize,
+    arr: *const xen_pfn_t,
+    err: *mut c_int,
+) -> *mut c_void;
+// xenforeignmemory_unmap
+type FnUnmap = fn(fmem: *mut xenforeignmemory_handle, addr: *mut c_void, pages: usize) -> c_int;
+
+#[derive(Debug)]
+pub struct LibXenForeignMemory {
+    lib: Library,
+    pub open: RawSymbol<FnOpen>,
+    pub close: RawSymbol<FnClose>,
+    pub map: RawSymbol<FnMap>,
+    pub unmap: RawSymbol<FnUnmap>,
+}
+
+impl LibXenForeignMemory {
+    pub unsafe fn new() -> Self {
+        info!("Loading {}", LIBXENFOREIGNMEMORY_FILENAME);
+        let lib = Library::new(LIBXENFOREIGNMEMORY_FILENAME).unwrap();
+        // load symbols
+        let open_sym: Symbol<FnOpen> = lib.get(b"xenforeignmemory_open\0").unwrap();
+        let open = open_sym.into_raw();
+
+        let close_sym: Symbol<FnClose> = lib.get(b"xenforeignmemory_close\0").unwrap();
+        let close = close_sym.into_raw();
+
+        let map_sym: Symbol<FnMap> = lib.get(b"xenforeignmemory_map\0").unwrap();
+        let map = map_sym.into_raw();
+
+        let unmap_sym: Symbol<FnUnmap> = lib.get(b"xenforeignmemory_unmap\0").unwrap();
+        let unmap = unmap_sym.into_raw();
+
+        LibXenForeignMemory {
+            lib,
+            open,
+            close,
+            map,
+            unmap,
+        }
+    }
+}
